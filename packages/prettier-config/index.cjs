8d03ceea07b20cc21197e617658e35a68be140cd
junkisai <jonki.beam@gmail.com>
2024-04-15 01:09:40 +0900

    chore: biome, prettier, stylelintの設定

---
+++ packages/prettier-config/index.cjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const config = {
+  bracketSpacing: true,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+}
+
+module.exports = config
