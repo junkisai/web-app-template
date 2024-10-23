6357b7dfb9475f896bd119fd0e7e790a76db62e6
Junki Saito <jonki.beam@gmail.com>
2024-10-24 01:28:50 +0900

    clean: delete prettier-plugin-astro

--- apps/admin/.prettierrc.cjs
+++ apps/admin/.prettierrc.cjs
@@ -3,7 +3,6 @@ const baseConf = require('@packages/prettier-config/index.cjs')
 /** @type {import('prettier').Config} */
 const config = {
   ...baseConf,
-  plugins: ['prettier-plugin-astro'],
 }
 
 module.exports = config
