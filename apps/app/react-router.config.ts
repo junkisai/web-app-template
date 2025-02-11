de06318e5777c1b77b570d9ca6660f483b54c6f7
Junki Saito <jonki.beam@gmail.com>
2025-02-12 00:37:19 +0900

    Migrate app and admin from Remix to React Router v7 (#111)

---
+++ apps/app/react-router.config.ts
@@ -0,0 +1,6 @@
+import type { Config } from '@react-router/dev/config'
+
+export default {
+  // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: true,
+} satisfies Config
