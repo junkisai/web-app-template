51645cdfe34d5854d67e506d23a7166dc105b7d1
Junki Saito <jonki.beam@gmail.com>
2025-05-10 21:15:45 +0900

    feat: add Button component to admin app with styled-system integration
    - Introduce a new Button component with customizable styles and variants
    - Update package.json to include ts-pattern as a dependency
    - Modify pnpm-lock.yaml to reflect new dependencies and versions

---
+++ apps/admin/src/components/ui/Button.tsx
@@ -0,0 +1,91 @@
+import Link from 'next/link'
+import type { FC, PropsWithChildren } from 'react'
+import { type RecipeVariantProps, cva } from 'styled-system/css'
+import { styled } from 'styled-system/jsx'
+import { match } from 'ts-pattern'
+
+const buttonStyle = cva({
+  base: {
+    display: 'inline-flex',
+    alignItems: 'center',
+    cursor: 'pointer',
+    borderRadius: 'md',
+  },
+  variants: {
+    variant: {
+      solid: {
+        bg: 'stone.800',
+        color: 'white',
+        _hover: {
+          bg: 'stone.700',
+        },
+        _active: {
+          bg: 'stone.600',
+        },
+      },
+      outline: {
+        borderWidth: '1px',
+        borderColor: 'stone.300',
+        _hover: {
+          bg: 'stone.100',
+        },
+        _active: {
+          bg: 'stone.200',
+        },
+      },
+    },
+    size: {
+      xs: {
+        h: '7',
+        px: '2.5',
+        fontSize: 'xs',
+      },
+      sm: {
+        h: '8',
+        px: '3.5',
+        fontSize: 'sm',
+      },
+      md: {
+        h: '9',
+        px: '4',
+        fontSize: 'md',
+      },
+      lg: {
+        h: '10',
+        px: '5',
+        fontSize: 'lg',
+      },
+    },
+  },
+  defaultVariants: {
+    variant: 'outline',
+    size: 'md',
+  },
+})
+
+type ButtonVariants = RecipeVariantProps<typeof buttonStyle>
+
+type Props = PropsWithChildren &
+  ButtonVariants &
+  (
+    | {
+        as?: 'button'
+      }
+    | {
+        as: 'a'
+        href: string
+      }
+  )
+
+export const Button: FC<Props> = ({ children, ...props }) => {
+  return match(props)
+    .with({ as: 'a' }, (props) => {
+      const ButtonLink = styled(Link, buttonStyle)
+
+      return <ButtonLink {...props}>{children}</ButtonLink>
+    })
+    .otherwise((props) => {
+      const Button = styled('button', buttonStyle)
+      return <Button {...props}>{children}</Button>
+    })
+}
