18e19349fcfd73f0bbce8b758cf4b2b3b198b2ae
Junki Saito <jonki.beam@gmail.com>
2025-06-07 19:19:48 +0900

    feat: refactor BaseLayout to use grid layout and wrap with ToastProvider

--- apps/admin/src/components/layouts/BaseLayout/BaseLayout.tsx
+++ apps/admin/src/components/layouts/BaseLayout/BaseLayout.tsx
@@ -1,23 +1,28 @@
+import { ToastProvider } from '@/components/ui/Toast'
 import type { FC, PropsWithChildren } from 'react'
 import { css } from 'styled-system/css'
 import { Sidebar } from './Sidebar'
 
 const wrapperStyle = css({
-  display: 'flex',
+  display: 'grid',
+  gridTemplateColumns: 'auto 1fr',
   gap: '10',
 })
 
 const mainStyle = css({
   pt: '10',
+  pr: '5',
 })
 
 type Props = PropsWithChildren
 
 export const BaseLayout: FC<Props> = ({ children }) => {
   return (
-    <div className={wrapperStyle}>
-      <Sidebar />
-      <div className={mainStyle}>{children}</div>
-    </div>
+    <ToastProvider>
+      <div className={wrapperStyle}>
+        <Sidebar />
+        <div className={mainStyle}>{children}</div>
+      </div>
+    </ToastProvider>
   )
 }
